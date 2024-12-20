5f5b843101849952babb56f2ef8f55d07b38d513
Houjun Liu <houjun@jemoka.com>
2024-12-19 22:15:16 -0800

    don't validate that often

--- parameters.py
+++ parameters.py
@@ -7,11 +7,12 @@ parser.add_argument("experiment", help="name for the experiment", type=str)
 parser.add_argument('-v', '--verbose', action='count', default=0, help="log level")
 parser.add_argument("--wandb", default=False, action="store_true", help="whether to use wandb")
 parser.add_argument("--seed", default=0, type=int, help="random seed")
+parser.add_argument("--warm_start", default=None, type=str, help="recover trainer from this path")
 
 # intervals
 parser.add_argument("--report_interval", default=64, type=int, help="save to wandb every this many steps")
-parser.add_argument("--checkpoint_interval", default=256, type=int, help="checkpoint every this many steps")
-parser.add_argument("--validation_interval", default=256, type=int, help="validate every this many steps")
+parser.add_argument("--checkpoint_interval", default=1024, type=int, help="checkpoint every this many steps")
+parser.add_argument("--validation_interval", default=2048, type=int, help="validate every this many steps")
 
 # dataset
 parser.add_argument("--out_dir", help="directory to save checkpoints and outputs", type=str, default="output")
