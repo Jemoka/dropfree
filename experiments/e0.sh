42da180a553385d162c11663309ae1f2e1150731
Houjun Liu <houjun@jemoka.com>
2024-12-19 23:54:56 -0800

    do things work?!

--- experiments/e0.sh
+++ experiments/e0.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-python main.py implement_me
-
+# this is just a dry run
+uvrun -q jag -d "a6000" -g 4 -r 32G -c 4 "source .venv/bin/activate &&  accelerate launch main.py e0_dryrun_160m_0.0 --batch_size 16 --wandb"
+uvrun -q jag -d "a6000" -g 4 -r 32G -c 4 "source .venv/bin/activate &&  accelerate launch main.py e0_dryrun_160m_0.5 --batch_size 16 --dropout 0.5 --wandb"
