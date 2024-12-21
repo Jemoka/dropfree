5a46ae34e95d77aabb4e8521ceb66a35878b2448
Houjun Liu <houjun@jemoka.com>
2024-12-21 15:37:47 -0800

    some scratchpad inference code

--- scratchpad.py
+++ scratchpad.py
@@ -23,14 +23,34 @@ from commands import configure
 
 from transformers import AutoConfig, AutoModelForCausalLM, AutoTokenizer
 
-config = AutoConfig.from_pretrained("EleutherAI/pythia-160m")
+trainer = Trainer.from_pretrained("./output/e0_dryrun_160m_0.0/best")
 
-config.attention_dropout = 0.7
-config.hidden_dropout = 0.7
-config.maximum_chicken = 0.1
-config
-config.max_position_embeddings
+model = trainer.model
+tokenizer = trainer.tokenizer
 
+result = model.generate(**tokenizer(["this is surprisingly "], return_tensors="pt").to("cuda"),
+                        do_sample=True,
+                        top_p=0.9,
+                        repetition_penalty=1.1,
+                        max_length=256)
+print(tokenizer.batch_decode(result)[0])
+
+
+
+
+
+# import os
+# os.listdir("./output")
+
+# config = AutoConfig.from_pretrained("EleutherAI/pythia-160m")
+
+# config.attention_dropout = 0.7
+# config.hidden_dropout = 0.7
+# config.maximum_chicken = 0.1
+# config
+# config.max_position_embeddings
+
+# print("ehwo")
 
 # model = AutoModelForCausalLM.from_config(config)
 # model
