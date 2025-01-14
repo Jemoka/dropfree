81d1bc373dbfc5540d7492c8626839d84c71ad65
Houjun Liu <houjun@jemoka.com>
2025-01-13 23:20:36 -0800

    blimp!

--- scratchpad.py
+++ scratchpad.py
@@ -17,19 +17,6 @@ logger.add(
     enqueue=True
 )
 
-from trainer import Trainer
+
 from commands import configure
-
-
-config = configure(
-    "test",
-    lr=1.2e-4,
-    batch_size=16,
-    architecture="EleutherAI/pythia-160m",
-    report_interval=1,
-    checkpoint_interval=16,
-    validation_interval=32
-)
-trainer = Trainer(config)
-trainer.train()
-
+from trainer import Trainer
